1453699347dec2ec93a0f43bd770d7b2fac87223
LY0528 <lyyan584521@163.com>
2018-03-28 17:36:00 +0800

    增加文章详情页，并且在文章详情页支持查看上一页和下一页

---
+++ src/views/themeData/ThemeData.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div class="basic-info">
+      <img :src="themeData.background" alt="">
+      <div class="intro font-18 font-color-fff">{{themeData.description}}</div>
+      <div class="name font-18 font-color-fff">————{{themeData.name}}</div>
+    </div>
+    <flexbox class="stories" orient="vertical">
+      <flexbox-item
+        v-for="item in themeData.stories"
+        :key="item.id">
+        <div class="item-list card-box-shadow">
+          <div class="left">
+            <img :src="item.images && item.images[0]" alt="">
+          </div>
+          <div class="right">{{item.title}}</div>
+        </div>
+      </flexbox-item>
+    </flexbox>
+  </div>
+</template>
+
+<script>
+  import { request } from '@/utils/request'
+  import { Flexbox, FlexboxItem } from 'vux'
+  export default {
+    components: {
+      Flexbox,
+      FlexboxItem
+    },
+    data () {
+      return {
+        themeData: {},
+        id: this.$route.params.id
+      }
+    },
+    created () {
+      this.getDetail()
+    },
+    methods: {
+      getDetail () {
+        request(`theme/${this.id}`, {
+
+        }).then(res => {
+          console.log(res.data)
+          this.themeData = res.data
+        }, error => {
+          console.log(error)
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .basic-info{
+    position: relative;
+    img{
+      display: block;
+      width: 100%;
+      height: 240px;
+      filter: opacity(70%);
+    }
+    .intro,
+    .name{
+      position: absolute;
+      right: 0;
+    }
+    .intro{
+      top: 80px;
+    }
+    .name{
+      bottom: 30px;
+    }
+  }
+  .stories{
+    .vux-flexbox-item{
+      margin-bottom: 10px;
+    }
+    .item-list{
+      padding: 10px;
+      background: #fff;
+      .left{
+        max-width: 64px;
+        max-height: 64px;
+        margin-right: 10px;
+        img{
+          display: block;
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
